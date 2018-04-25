ec4d943a4a2d47529f06a797bd33c1844e2e5746
Rafael Passarela <rafaelpassarela@gmail.com>
2018-04-25 01:43:55 -0300

    Start of MainActivity
    Some basic init

--- SayIt_TextToSpeech/MainActivity.cs
+++ SayIt_TextToSpeech/MainActivity.cs
@@ -1,12 +1,46 @@
-﻿using Android.App;
+﻿using System;
+
+using Android.App;
+using Android.Content;
 using Android.Widget;
 using Android.OS;
+using Android.Speech.Tts;
+using System.Collections.Generic;
+using System.Linq;
+using Android.Runtime;
+using Java.Util;
 
 namespace SayIt_TextToSpeech
 {
-    [Activity(Label = "SayIt_TextToSpeech", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity, TextToSpeech.IOnInitListener
     {
+        private TextToSpeech textToSpeech;
+        private Locale lang;
+        private readonly int needLang = 103;
+
+        public void OnInit([GeneratedEnum] OperationResult status)
+        {
+            // if get error, set default language
+            if (status == OperationResult.Error)
+                textToSpeech.SetLanguage(Java.Util.Locale.Default);
+            // listener ok, set lang
+            if (status == OperationResult.Success)
+                textToSpeech.SetLanguage(lang);
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            // base.OnActivityResult(requestCode, resultCode, data);
+            if (requestCode == needLang)
+            {
+                // we need a new language installed
+                var installTTS = new Intent();
+                installTTS.SetAction(TextToSpeech.Engine.ActionInstallTtsData);
+                StartActivity(installTTS);
+            }
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
--- SayIt_TextToSpeech/Resources/Resource.Designer.cs
+++ SayIt_TextToSpeech/Resources/Resource.Designer.cs
@@ -41,6 +41,76 @@ namespace SayIt_TextToSpeech
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040012
+			public const int btnSpeak = 2130968594;
+			
+			// aapt resource value: 0x7f040002
+			public const int editSpeech = 2130968578;
+			
+			// aapt resource value: 0x7f040001
+			public const int lblSayText = 2130968577;
+			
+			// aapt resource value: 0x7f040003
+			public const int linearLayout2 = 2130968579;
+			
+			// aapt resource value: 0x7f040011
+			public const int linearLayout3 = 2130968593;
+			
+			// aapt resource value: 0x7f040000
+			public const int linearLayoutMain = 2130968576;
+			
+			// aapt resource value: 0x7f04000b
+			public const int seekPitch = 2130968587;
+			
+			// aapt resource value: 0x7f04000f
+			public const int seekSpeed = 2130968591;
+			
+			// aapt resource value: 0x7f040007
+			public const int spinLanguage = 2130968583;
+			
+			// aapt resource value: 0x7f040004
+			public const int tableLayout1 = 2130968580;
+			
+			// aapt resource value: 0x7f040005
+			public const int tableRow1 = 2130968581;
+			
+			// aapt resource value: 0x7f040009
+			public const int tableRow2 = 2130968585;
+			
+			// aapt resource value: 0x7f04000d
+			public const int tableRow3 = 2130968589;
+			
+			// aapt resource value: 0x7f04000c
+			public const int textPitch = 2130968588;
+			
+			// aapt resource value: 0x7f040010
+			public const int textSpeed = 2130968592;
+			
+			// aapt resource value: 0x7f040006
+			public const int textView11 = 2130968582;
+			
+			// aapt resource value: 0x7f04000a
+			public const int textView12 = 2130968586;
+			
+			// aapt resource value: 0x7f04000e
+			public const int textView13 = 2130968590;
+			
+			// aapt resource value: 0x7f040008
+			public const int textView14 = 2130968584;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
@@ -62,6 +132,15 @@ namespace SayIt_TextToSpeech
 			
 			// aapt resource value: 0x7f030000
 			public const int app_name = 2130903040;
+			
+			// aapt resource value: 0x7f030003
+			public const int say_button = 2130903043;
+			
+			// aapt resource value: 0x7f030002
+			public const int say_input = 2130903042;
+			
+			// aapt resource value: 0x7f030001
+			public const int say_label = 2130903041;
 			
 			static String()
 			{
